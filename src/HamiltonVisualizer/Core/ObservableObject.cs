db24102e860f630edb204d62507042f82aab1010
dtHvinh <dangthehienvinh662@gmail.com>
2024-01-21 15:36:47 +0700

    Add ObservableObject and RelayCommand

---
+++ src/HamiltonVisualizer/Core/ObservableObject.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HamiltonVisualizer.Core
+{
+    /// <summary>
+    /// An object that can be tracked by the binding client.
+    /// </summary>
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
