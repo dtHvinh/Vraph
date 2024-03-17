6f046bbb4b468c2c8bdeaf16337e7a57a169c13e
dtHvinh <dangthehienvinh662@gmail.com>
2024-03-18 06:24:59 +0700

    Add constants

--- src/HamiltonVisualizer/Constants/ControlConstants.cs
+++ src/HamiltonVisualizer/Constants/ControlConstants.cs
@@ -3,6 +3,6 @@
     public static class ControlConstants
     {
         public static readonly (int, int, int, int) ModeButtonMarginDefault = (-37, 0, 0, 0);
-        public static readonly (int, int, int, int) ModeButtonOn = (37, 0, 0, 0);
+        public static readonly (int, int, int, int) ModeButtonOn = (39, 0, 0, 0);
     }
 }
