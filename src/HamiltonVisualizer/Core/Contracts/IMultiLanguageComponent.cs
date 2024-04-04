6f8cf7661c7c2871845c41d8e949a9f90ac6299f
dtHvinh <dangthehienvinh662@gmail.com>
2024-04-04 10:01:18 +0700

    Add multi lang support interface

---
+++ src/HamiltonVisualizer/Core/Contracts/IMultiLanguageComponent.cs
@@ -0,0 +1,7 @@
+﻿namespace HamiltonVisualizer.Core.Contracts
+{
+    public interface IMultiLanguageComponent // TODO: improve this
+    {
+        string ToString(string lang); // TODO: use enum istead
+    }
+}
