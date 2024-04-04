56ce8f3b60b463d970ec2d2bd8aafee2065f22d4
dtHvinh <dangthehienvinh662@gmail.com>
2024-04-04 10:01:50 +0700

    Add IRef.. interfaces

---
+++ src/HamiltonVisualizer/Core/Contracts/IRefGetter.cs
@@ -0,0 +1,33 @@
+﻿namespace HamiltonVisualizer.Core.Contracts
+{
+    public interface IRefGetter<T1>
+    {
+        T1 GetRef();
+    }
+
+    public interface IRefGetter<T1, T2>
+    {
+        (T1, T2) GetRefs();
+    }
+
+    public interface IRefGetter<T1, T2, T3>
+    {
+        (T1, T2, T3) GetRefs();
+    }
+
+
+    public interface IReadOnlyRefGetter<T1>
+    {
+        T1 GetReadOnlyRef();
+    }
+
+    public interface IReadOnlyRefGetter<T1, T2>
+    {
+        (T1, T2) GetReadOnlyRefs();
+    }
+
+    public interface IReadOnlyRefGetter<T1, T2, T3>
+    {
+        (T1, T2, T3) GetReadOnlyRefs();
+    }
+}
